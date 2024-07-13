155fa0fc2829dded3282726aef2db23c13ba141f
HoangThaiDev <thaindev2000@gmail.com>
2024-07-13 19:32:51 +0700

    Update constant.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 /** Những domain được phép truy cập tới tài nguyên của server */
-exports.WHITELIST_DOMAINS = ["http://localhost:3000"];
+exports.WHITELIST_DOMAINS = ["https://grocery-eg.vercel.app/"];
