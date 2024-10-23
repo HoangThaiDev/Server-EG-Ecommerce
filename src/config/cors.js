d963c033e7ac5ba4a392a6ae8f95379d136bfb83
HoangThaiDev <thaindev2000@gmail.com>
2024-10-23 14:44:23 +0700

    test deploy x8

--- src/config/cors.js
+++ src/config/cors.js
@@ -8,19 +8,16 @@ const env = require("./enviroment");
 exports.corsOptions = {
   origin: function (origin, callback) {
     // Nếu môi trường là dev, cho phép tất cả các origin trong whitelist
-    if (env.BUILD_MODE === "dev") {
-      if (!origin || WHITELIST_DOMAINS_DEV.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS")); // Không cho phép
+    if (env.BUILD_MODE === "dev" && WHITELIST_DOMAINS_DEV.includes(origin)) {
+      return callback(null, true);
     }
 
     // Nếu môi trường là production, kiểm tra xem origin có trong whitelist không
-    if (env.BUILD_MODE === "production") {
-      if (WHITELIST_DOMAINS_PRODUCTION.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
+    if (
+      env.BUILD_MODE === "production" &&
+      WHITELIST_DOMAINS_PRODUCTION.includes(origin)
+    ) {
+      return callback(null, true);
     }
     // Trường hợp không hợp lệ (thường không xảy ra trong dev)
     callback(new Error("Not allowed by CORS"));
