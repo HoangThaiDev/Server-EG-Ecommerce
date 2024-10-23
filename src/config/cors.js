d7400865b863df24499808af431c8c36e89ea34a
HoangThaiDev <thaindev2000@gmail.com>
2024-10-23 12:13:03 +0700

    Test deploy x2

--- src/config/cors.js
+++ src/config/cors.js
@@ -7,6 +7,8 @@ const env = require("./enviroment");
 
 exports.corsOptions = {
   origin: function (origin, callback) {
+    console.log("origin", origin);
+
     // Cho phép tất cả trong môi trường dev
     if (env.BUILD_MODE === "dev" && WHITELIST_DOMAINS_DEV.includes(origin)) {
       return callback(null, true);
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 /** Những domain được phép truy cập tới tài nguyên của server ở môi trường production*/
-exports.WHITELIST_DOMAINS_PRODUCTION = ["https://eg-grocery.vercel.app/"];
+exports.WHITELIST_DOMAINS_PRODUCTION = ["https://eg-grocery.vercel.app"];
 
 /** Những domain được phép truy cập tới tài nguyên của server ở môi trường dev*/
 exports.WHITELIST_DOMAINS_DEV = ["http://localhost:3000"];
